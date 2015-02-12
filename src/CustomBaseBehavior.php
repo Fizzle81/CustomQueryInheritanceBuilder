cf0fbb08faaf3cdac43f53e3964ab1bc37c64347
Christoph Quadt <quadt@united-domains.de>
2015-02-12 16:08:13 +0100

    modifications for base behavior

--- src/CustomBaseBehavior.php
+++ src/CustomBaseBehavior.php
@@ -1,35 +1,51 @@
 <?php
 
+namespace CustomBaseBehavior\Behavior;
+
+use Propel\Generator\Model\Behavior;
 /**
- * CustomBaseBehavior
+ * custom base behavior for propel 2
  *
- * @author Charles SANQUER - <charles.sanquer@gmail.com>
+ * @author Christoph Quadt <christoph@quadtrilogy.de>
  */
-class CustomBaseBehavior extends Behavior
-{
+class CustomBaseBehavior extends Behavior {
+
     // default parameters value
     protected $parameters = array(
         'base_query' => 'ModelCriteria',
     );
 
-    protected function cleanfullyQualifiedClassName($fullyQualifiedClassName)
-    {
+    /**
+     * clean fully qualified class name
+     *
+     * @param string $fullyQualifiedClassName the fully qualified class name
+     */
+    protected function cleanfullyQualifiedClassName($fullyQualifiedClassName) {
         return trim(str_replace(array('.', '/'), '\\', $fullyQualifiedClassName), '\\');
     }
 
-    protected function getClassName($fullyQualifiedClassName)
-    {
+    /**
+     * get class name
+     *
+     * @param string $fullyQualifiedClassName the fully qualified class name
+     *
+     * @return string
+     */
+    protected function getClassName($fullyQualifiedClassName) {
         $className = $fullyQualifiedClassName;
         if (($pos = strrpos($fullyQualifiedClassName, '\\')) !== false) {
             $className = substr($fullyQualifiedClassName, $pos + 1);
             $namespace = substr($fullyQualifiedClassName, 0, $pos);
         }
-
         return $className;
     }
 
-    public function parentClass($builder)
-    {
+    /**
+     * get the parent class
+     *
+     * @param unknown $builder
+     */
+    public function parentClass($builder) {
         switch (get_class($builder)) {
             case 'QueryBuilder':
                 $class = $this->getParameter('base_query');
